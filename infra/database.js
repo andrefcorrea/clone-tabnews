9913dd7f81c1ae860ff78ca14e00f1735de4bbdb
andrefcorrea <49352522+andrefcorrea@users.noreply.github.com>
2025-06-12 00:04:51 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,16 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: true,
   });
-  console.log("Credenviais do Postgres:", {
+  console.log("Credenciais do Postgres:", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  
+
   try {
     await client.connect();
 
@@ -24,7 +25,6 @@ async function query(queryObject) {
   } catch (error) {
     console.error(error);
     throw error;
-    
   } finally {
     await client.end();
   }
